4bf9553a0e75396733fd47f5076d596bbed2947f
csepankajsen <cse.pankajsen@gmail.com>
2023-07-17 01:22:42 +0530

    "Container,Decoration and gradient"

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(209, 94, 94, 243),
-        body: Center(
-          child: Text('My First app'),
+        // backgroundColor: Color.fromARGB(209, 94, 94, 243),
+
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(152, 121, 180, 238),
+                Color.fromARGB(183, 2, 33, 65)
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('My First app'),
+          ),
         ),
       ),
     ),
