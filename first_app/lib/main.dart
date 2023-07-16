3a17dcc00f7231a70a4746c9a396c553a49f59c1
csepankajsen <cse.pankajsen@gmail.com>
2023-07-17 01:04:36 +0530

    "setting background color with center text "

---
+++ first_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(209, 94, 94, 243),
+        body: Center(
+          child: Text('My First app'),
+        ),
+      ),
+    ),
+  );
+}
