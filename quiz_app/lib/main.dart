87f50a0e5a336652e79f9bb53c23bb951f7f5c0b
csepankajsen <cse.pankajsen@gmail.com>
2023-07-24 22:48:54 +0530

    "Added statefull widgets for quiz page and questions scrren page with starting chnages not full"

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
+
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(252, 244, 214, 96),
-                Color.fromARGB(248, 143, 249, 5)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+    const Quiz(),
   );
 }
---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(252, 244, 214, 96),
+                Color.fromARGB(248, 143, 249, 5)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
