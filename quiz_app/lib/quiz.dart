a24f1d03dfc6d2108c6aa5e979f81b607e413be3
csepankajsen <cse.pankajsen@gmail.com>
2023-07-25 10:46:19 +0530

    "Implementtation of stateful widget; Switiching of screen from calling function from one widget to another(alivate a state); Use of initStatefunction to prior initialize any thing before build is called; Passing function in arguments;

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -12,9 +12,21 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget activeScreen = const StartScreen(); //storingt widget in a variable type widget
+  // Widget activeScreen = StartScreen(switchScreen); //storingt widget in a variable type widget but this will show error because it is being initialized before call so to over come this we need to use inbuilt initstate method like below
+  Widget? activeScreen;//using question mark to tell trhat initially this can be null also
+
+  @override // this will initialize first
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
   void switchScreen() {
+    setState(() {
+      
     activeScreen = const QuestionsScreen();
+      
+    });
   }
 
   @override
--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 // import 'package:quiz_app/wallpaper.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  // const StartScreen(void Function() StartQuiz, {super.key});//can also pass function like this
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
   @override
   Widget build(context) {
     return Center(
@@ -26,7 +29,7 @@ class StartScreen extends StatelessWidget {
           // Icon(),---can also use icon outside of the button
           const SizedBox(height: 20),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(foregroundColor: const Color.fromARGB(255, 228, 10, 13)),
             // icon: Text()
             icon: const Icon(Icons.arrow_circle_right_outlined),
